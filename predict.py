2d7c2a72efa131844ae4c720aa1239994f1fb960
hn <csuhuangneng@gmail.com>
2018-10-26 15:38:57 +0800

    修改predict.py代码，增加测试脚本

--- predict.py
+++ predict.py
@@ -21,13 +21,17 @@ import shutil
 import numpy as np
 from model import Model
 from linedecode import decode_line
+from sklearn import metrics
 
 
 def predict(argv):
     batch_size = argv.batch_size
     init_learning_rate = argv.learning_rate
     class_num = argv.class_num
-    LOG_DIR = argv.output_log_dir
+    # l1_scale = argv.l1_scale
+    # l2_scale = argv.l2_scale
+    MODLE_DIR = argv.model_dir
+    MODEL_NAME = argv.model_name+'.ckpt'
 
     FEATURE_LEN = argv.base_num
     SIGNAL_LEN = argv.signal_num
@@ -38,19 +42,21 @@ def predict(argv):
     signal_bytes = SIGNAL_LEN * 4
     label_bytes = 1
     rnames_bytes = argv.max_rname_len * 1
-    record_len = base_bytes + means_bytes + stds_bytes + sanum_bytes + signal_bytes + label_bytes + rnames_bytes
+    record_len = base_bytes + means_bytes + stds_bytes + \
+        sanum_bytes + signal_bytes + label_bytes + rnames_bytes
 
     files = []
     for file in os.listdir(argv.input_dir):
         files.append(argv.input_dir + '/' + file)
 
-    model = Model(base_num=FEATURE_LEN, signal_num=SIGNAL_LEN, class_num=class_num)
+    model = Model(base_num=FEATURE_LEN,
+                  signal_num=SIGNAL_LEN, class_num=class_num)
 
     fwrite = open(argv.result_file, 'w')
 
     with tf.Session() as sess:
         saver = tf.train.Saver()
-        saver.restore(sess, LOG_DIR + 'final.ckpt')
+        saver.restore(sess, MODLE_DIR + MODEL_NAME)
         accuracy_list = []
         dataset = tf.data.FixedLengthRecordDataset(files, record_len).map(
             lambda x: decode_line(value=x, base_num=FEATURE_LEN, signal_num=SIGNAL_LEN, rname_len=argv.max_rname_len))
@@ -73,8 +79,10 @@ def predict(argv):
                              model.lr: init_learning_rate,
                              model.training: False,
                              model.keep_prob: 1.0}
-                activation_logits, prediction, accuracy = sess.run(
-                    [model.activation_logits, model.prediction, model.accuracy], feed_dict=feed_dict)
+                activation_logits, prediction = sess.run(
+                    [model.activation_logits, model.prediction], feed_dict=feed_dict)
+                accuracy = metrics.accuracy_score(
+                    y_true=label, y_pred=prediction)
                 for idx in range(label.shape[0]):
                     fwrite.write(bytes.decode(rnames[idx]) + '\t' + str(activation_logits[idx][0]) + '\t' + str(
                         activation_logits[idx][1]) + '\t' + str(prediction[idx]) + '\t' + str(label[idx]) + '\n')
@@ -91,8 +99,9 @@ if __name__ == "__main__":
     parser.add_argument('-l', '--learning_rate', default=0.001, type=float)
     parser.add_argument('-c', '--class_num', default=2, type=int)
     parser.add_argument('-i', '--input_dir', required=True)
-    parser.add_argument('-o', '--output_log_dir', default='./log/')
+    parser.add_argument('-o', '--model_dir', required=True)
     parser.add_argument('-r', '--result_file', required=True)
+    parser.add_argument('-n', '--model_name', default='5')
     parser.add_argument('-x', '--base_num', default=17, type=int)
     parser.add_argument('-y', '--signal_num', default=120, type=int)
     parser.add_argument('-z', '--max_rname_len', default=100, type=int)
---
+++ scripts/run_predict_cgi_21mer_mixture.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+'''
+/*
+ * @Author: huangneng 
+ * @Date: 2018-10-26 15:27:23 
+ * @Last Modified by: huangneng
+ * @Last Modified time: 2018-10-26 15:34:22
+ */
+'''
+
+import os
+
+model_path = "/home/huangneng/master_deepsignal/deepsignal/last_v_models/cgi_21mer_mixture"
+result_path = "/home/huangneng/master_deepsignal/deepsignal/last_v_results/cgi_21mer_mixture"
+test_dir = "/home/huangneng/data/human_hx1.test.bn21.5xs/BJXWZ.x.hc_pos/hx1.test.bn21.BJXWZ_C789101112D1E13.cgi.17_mcgs"
+model_name = '19'
+
+test_path = '/home/huangneng/data/human_hx1.test.bn21.5xs/BJXWZ.x.hc_pos/hx1.test.bn21.BJXWZ_C789101112D1E13.cgi.17_mcgs'
+out_name = result_path+'/result.txt'
+# print(test_path)
+# print(out_name)
+cmd = "CUDA_VISIBLE_DEVICES=1 python ../predict.py -i {input}/ -o {model}/ -r {output} -n {modelname} -x 21 -y 512 -z 60 ".format(
+    input=test_path, model=model_path, output=out_name, modelname=model_name)
+print(cmd)
+os.system(cmd)
