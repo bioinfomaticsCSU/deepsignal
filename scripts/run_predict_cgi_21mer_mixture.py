76f9adffbb7703e71c4a8e5f6903d0460ef125de
hn <csuhuangneng@gmail.com>
2018-10-27 17:37:51 +0800

    predict new 21mer data

--- scripts/run_predict_cgi_21mer_mixture.py
+++ scripts/run_predict_cgi_21mer_mixture.py
@@ -4,7 +4,7 @@
  * @Author: huangneng 
  * @Date: 2018-10-26 15:27:23 
  * @Last Modified by: huangneng
- * @Last Modified time: 2018-10-26 15:34:22
+ * @Last Modified time: 2018-10-27 17:37:00
  */
 '''
 
@@ -12,10 +12,12 @@ import os
 
 model_path = "/home/huangneng/master_deepsignal/deepsignal/last_v_models/cgi_21mer_mixture"
 result_path = "/home/huangneng/master_deepsignal/deepsignal/last_v_results/cgi_21mer_mixture"
-test_dir = "/home/huangneng/data/human_hx1.test.bn21.5xs/BJXWZ.x.hc_pos/hx1.test.bn21.BJXWZ_C789101112D1E13.cgi.17_mcgs"
+# test_dir = "/home/huangneng/data/human_hx1.test.bn21.5xs/BJXWZ.x.hc_pos/hx1.test.bn21.BJXWZ_C789101112D1E13.cgi.17_mcgs"
+test_dir = "/home/huangneng/data/human_hx1.test.bn21.5xs/BJXWZ.x.hc_pos/hx1.test.bn21.BJXWZ_C789101112D1E13.cgi.mcgs"
 model_name = '19'
 
-test_path = '/home/huangneng/data/human_hx1.test.bn21.5xs/BJXWZ.x.hc_pos/hx1.test.bn21.BJXWZ_C789101112D1E13.cgi.17_mcgs'
+# test_path = '/home/huangneng/data/human_hx1.test.bn21.5xs/BJXWZ.x.hc_pos/hx1.test.bn21.BJXWZ_C789101112D1E13.cgi.17_mcgs'
+test_path = '/home/huangneng/data/human_hx1.test.bn21.5xs/BJXWZ.x.hc_pos/hx1.test.bn21.BJXWZ_C789101112D1E13.cgi.mcgs'
 out_name = result_path+'/result.txt'
 # print(test_path)
 # print(out_name)
