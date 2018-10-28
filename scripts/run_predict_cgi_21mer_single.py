bd9025ad7af55c62b9f474379c1a32cb45086c88
hn <csuhuangneng@gmail.com>
2018-10-28 11:22:54 +0800

    fix bug，去掉路径中的空格

--- scripts/run_predict_cgi_21mer_single.py
+++ scripts/run_predict_cgi_21mer_single.py
@@ -4,7 +4,7 @@
  * @Author: huangneng 
  * @Date: 2018-10-26 15:27:23 
  * @Last Modified by: huangneng
- * @Last Modified time: 2018-10-28 11:19:16
+ * @Last Modified time: 2018-10-28 11:22:26
  */
 '''
 
@@ -12,10 +12,10 @@ import os
 
 model_path = "/home/huangneng/master_deepsignal/deepsignal/last_v_models/cgi_21mer_single"
 result_path = "/home/huangneng/master_deepsignal/deepsignal/last_v_results/cgi_21mer_single"
-test_dir = "/home/huangneng/data/human_hx1.test.bn21.5xs/BJXWZ.x.hc_pos/ hx1.test.bn21.BJXWZ_C789101112D1E13.cgi.scgs"
+test_dir = "/home/huangneng/data/human_hx1.test.bn21.5xs/BJXWZ.x.hc_pos/hx1.test.bn21.BJXWZ_C789101112D1E13.cgi.scgs"
 model_name = '99'
 
-test_path = '/home/huangneng/data/human_hx1.test.bn21.5xs/BJXWZ.x.hc_pos/ hx1.test.bn21.BJXWZ_C789101112D1E13.cgi.scgs'
+test_path = '/home/huangneng/data/human_hx1.test.bn21.5xs/BJXWZ.x.hc_pos/hx1.test.bn21.BJXWZ_C789101112D1E13.cgi.scgs'
 out_name = result_path+'/result.txt'
 # print(test_path)
 # print(out_name)
