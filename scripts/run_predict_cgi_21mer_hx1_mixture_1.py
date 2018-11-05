ad6654a2db44615d41aefc6d3c1c78e566ce014e
hn <csuhuangneng@gmail.com>
2018-11-05 10:30:18 -0500

    在cpg岛上进行预测，预测数据是hx1的第一个10层cpg岛上的数据

---
+++ scripts/run_predict_cgi_21mer_hx1_mixture_1.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+'''
+/*
+ * @Author: huangneng 
+ * @Date: 2018-10-26 15:27:23 
+ * @Last Modified by: huangneng
+ * @Last Modified time: 2018-10-28 11:22:26
+ */
+'''
+
+import os
+
+model_path = "/home/huangneng/master_deepsignal/deepsignal/last_v_models/cgi_21mer_mixture"
+result_path = "/home/huangneng/master_deepsignal/deepsignal/last_v_results/cgi_21mer_hx1_mixture_1"
+test_dir = "/home/huangneng/data/human_hx1.test.bn21.5xs/BJXWZ.cgi.1.C789101112D1E13"
+model_name = '19'
+
+for subdir in os.listdir(test_dir):
+    if not subdir.endswith(".mcgs"):
+        continue
+    test_path = os.path.join(test_dir, subdir)
+    out_name = os.path.join(result_path, '.'.join(subdir.split('.'))+'.result')
+    # print(test_path)
+    # print(out_name)
+    cmd = "CUDA_VISIBLE_DEVICES=1 python ../predict.py -i {input}/ -o {model}/ -r {output} -n {modelname} -x 17 -y 375 -z 60 ".format(
+        input=test_path, model=model_path, modelname=model_name, output=out_name)
+    print(cmd)
+    os.system(cmd)
