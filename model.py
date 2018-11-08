cd3a967a2b8c88b8e6de1fcc7e00dd9412f07fc2
hn <csuhuangneng@gmail.com>
2018-11-08 14:06:06 -0500

    修改模型，去掉signal model，只用event model

--- model.py
+++ model.py
@@ -105,6 +105,15 @@ class Model():
             self.global_step = tf.get_variable('global_step', trainable=False, shape=(), dtype=tf.int32,
                                                initializer=tf.zeros_initializer())
 
+        with tf.name_scope('models'):
+            self.event_model = Event_model(sequence_len=tf.ones_like(self.labels) * base_num,
+                                           layer_num=3,
+                                           hidden_num=256,
+                                           keep_prob=self.keep)
+            self.signal_model = incept_net()
+            self.join_model = Joint_model(output_hidden=class_num,
+                                          keep_prob=self.keep_prob)
+
         with tf.name_scope("data_transfer"):
             one_hot_labels = tf.one_hot(self.labels, depth=class_num)
             W = tf.get_variable("embedding", shape=[vocab_size, embedding_size], dtype=tf.float32,
@@ -117,166 +126,18 @@ class Model():
                                             self.stds, [-1, base_num, 1]),
                                         tf.reshape(self.sanums, [-1, base_num, 1])], axis=2)
             signals = tf.reshape(self.signals, [-1, 1, signal_num, 1])
+
         with tf.name_scope("Event_model"):
             input_event = fusion_vector1
-            rnn_out, rnn_states = rnn_layers(input_event, tf.ones_like(self.labels) * base_num, hidden_num=256,
-                                             layer_num=3,
-                                             kprob=self.keep_prob)
-            self.fw_out = rnn_out[0]
-            self.bw_out = rnn_out[1]
-            extract_rnn_out = tf.concat(
-                [rnn_out[0][:, -1, :], rnn_out[1][:, 0, :]], axis=1)  # [batch,2*hidden_num]
-            extract_rnn_shape = extract_rnn_out.get_shape().as_list()
-            event_model_output = extract_rnn_out
+            event_model_output = self.event_model(input_event)
 
         with tf.name_scope("Signal_model"):
-            input_signal = signals
-
-            ############################################################################################################
-            # x = tf.layers.conv2d(inputs=input_signal, filters=16, kernel_size=[1, 3], strides=1, padding="SAME",
-            #                      use_bias=False,
-            #                      name='conv1')
-            # x = Batch_Normalization(x, is_training=self.training, scope='bn1')
-            # x = tf.nn.relu(x)
-            # x = tf.layers.max_pooling2d(x, [1, 2], strides=2, padding="SAME", name='maxpool1')
-            #
-            # x = tf.layers.conv2d(inputs=x, filters=32, kernel_size=[1, 3], strides=1, padding="SAME", use_bias=False,
-            #                      name='conv2')
-            # x = Batch_Normalization(x, is_training=self.training, scope='bn2')
-            # x = tf.nn.relu(x)
-            # x = tf.layers.max_pooling2d(x, [1, 2], strides=2, padding="SAME", name='maxpool2')
-            #
-            # x = tf.layers.conv2d(inputs=x, filters=64, kernel_size=[1, 3], strides=1, padding="SAME", use_bias=False,
-            #                      name='conv3')
-            # x = Batch_Normalization(x, is_training=self.training, scope='bn3')
-            # x = tf.nn.relu(x)
-            # x = tf.layers.max_pooling2d(x, [1, 2], strides=2, padding="SAME", name='maxpool3')
-            #
-            # x = tf.layers.conv2d(inputs=x, filters=128, kernel_size=[1, 3], strides=1, padding="SAME", use_bias=False,
-            #                      name='conv4')
-            # x = Batch_Normalization(x, is_training=self.training, scope='bn4')
-            # x = tf.nn.relu(x)
-            # x = tf.layers.max_pooling2d(x, [1, 2], strides=2, padding="SAME", name='maxpool4')
-            #
-            # x_shape = x.get_shape().as_list()
-            # signal_model_output = tf.reshape(x, [-1, x_shape[2] * x_shape[3]])  # [batch,1024]
-            ############################################################################################################
-            with tf.variable_scope("conv_layer1"):
-                x = tf.layers.conv2d(inputs=input_signal, filters=64, kernel_size=[1, 7], strides=2, padding="SAME",
-                                     use_bias=False, name="conv")
-                x = Batch_Normalization(
-                    x, is_training=self.training, scope='bn')
-                x = tf.nn.relu(x)  # [188,64]
-            with tf.variable_scope("maxpool_layer1"):
-                x = tf.layers.max_pooling2d(
-                    x, [1, 3], strides=2, padding="SAME", name="maxpool")  # [94,64]
-            with tf.variable_scope("conv_layer2"):
-                x = tf.layers.conv2d(inputs=x, filters=128, kernel_size=[1, 1], strides=1, padding="SAME",
-                                     use_bias=False, name="conv")
-                x = Batch_Normalization(
-                    x, is_training=self.training, scope='bn')
-                x = tf.nn.relu(x)  # [94,128]
-            with tf.variable_scope("conv_layer3"):
-                x = tf.layers.conv2d(inputs=x, filters=256, kernel_size=[1, 3], strides=1, padding="SAME",
-                                     use_bias=False, name="conv")
-                x = Batch_Normalization(
-                    x, is_training=self.training, scope='bn')
-                x = tf.nn.relu(x)  # [94,256]
-            # inception layer x 11
-            with tf.variable_scope('incp_layer1'):
-                x = inception_layer(x, self.training)  # [94,192]
-            with tf.variable_scope('incp_layer2'):
-                x = inception_layer(x, self.training)
-            with tf.variable_scope('incp_layer3'):
-                x = inception_layer(x, self.training)
-            with tf.variable_scope('maxpool_layer2'):
-                x = tf.layers.max_pooling2d(
-                    x, [1, 3], strides=2, padding="SAME", name="maxpool")   # [47,192]
-            with tf.variable_scope('incp_layer4'):
-                x = inception_layer(x, self.training)
-            with tf.variable_scope('incp_layer5'):
-                x = inception_layer(x, self.training)
-            with tf.variable_scope('incp_layer6'):
-                x = inception_layer(x, self.training)
-            with tf.variable_scope('incp_layer7'):
-                x = inception_layer(x, self.training)
-            with tf.variable_scope('incp_layer8'):
-                x = inception_layer(x, self.training)
-            with tf.variable_scope('maxpool_layer3'):
-                x = tf.layers.max_pooling2d(
-                    x, [1, 3], strides=2, padding="SAME", name="maxpool")   # [24,192]
-            with tf.variable_scope('incp_layer9'):
-                x = inception_layer(x, self.training)
-            with tf.variable_scope('incp_layer10'):
-                x = inception_layer(x, self.training)
-            with tf.variable_scope('incp_layer11'):
-                x = inception_layer(x, self.training)
-            with tf.variable_scope('avgpool_layer1'):
-                x = tf.layers.average_pooling2d(
-                    x, [1, 7], strides=1, padding="SAME", name="avgpool")   # [24,192]
-            print('inception output shape:', x.get_shape().as_list())
-            x_shape = x.get_shape().as_list()
-            signal_model_output = tf.reshape(x, [-1, x_shape[2] * x_shape[3]])
-            ############################################################################################################
-            # x = create_inception_resnet_v1(indata=input_signal,training=self.training,keep_prob=self.keep_prob)
-            # print('inception output shape:', x.get_shape().as_list())
-            # x_shape = x.get_shape().as_list()
-            # signal_model_output = tf.reshape(x, [-1, x_shape[2] * x_shape[3]])
-
-            ############################################################################################################
-            # x = residual_cnn(indata=input_signal, out_channel=64, training=self.is_training, k=7, stride=2, scope="res_cnn_1")  # [N,188,64]
-            # x = tf.layers.max_pooling2d(x, pool_size=[1, 3], strides=2, padding="SAME", name='mxp_1')  # [N,94,64]
-            # x = residual_cnn(indata=x, out_channel=64, training=self.is_training, k=3, stride=1, scope="res_cnn_2")  # [N,94,64]
-            #
-            # x = residual_cnn(indata=x, out_channel=128, training=self.is_training, k=3, stride=2, scope="res_cnn_3")  # [N,47,128]
-            # x = residual_cnn(indata=x, out_channel=128, training=self.is_training, k=3, stride=1, scope="res_cnn_4")  # [N,47,128]
-            #
-            # x = residual_cnn(indata=x, out_channel=256, training=self.is_training, k=3, stride=2, scope="res_cnn_5")  # [N,24,256]
-            # x = residual_cnn(indata=x, out_channel=256, training=self.is_training, k=3, stride=1, scope="res_cnn_6")  # [N,24,256]
-            # x_shape = x.get_shape().as_list()
-            # signal_model_output = tf.reshape(x, [x_shape[0], x_shape[2] * x_shape[3]])  # [batch,6144]
-
-            ############################################################################################################
-
-            # x = residual_cnn(indata=input_signal, out_channel=16, training=self.training, k=7, stride=2,
-            #                  scope="res_cnn_1")  # [N,188,64]
-            # x = tf.layers.max_pooling2d(x, pool_size=[1, 3], strides=2, padding="SAME", name='mxp_1')  # [N,94,64]
-            # x = residual_cnn(indata=x, out_channel=16, training=self.training, k=3, stride=1,
-            #                  scope="res_cnn_2")  # [N,94,64]
-
-            # x = residual_cnn(indata=x, out_channel=32, training=self.training, k=3, stride=2,
-            #                  scope="res_cnn_3")  # [N,47,128]
-            # x = residual_cnn(indata=x, out_channel=32, training=self.training, k=3, stride=1,
-            #                  scope="res_cnn_4")  # [N,47,128]
-
-            # x = residual_cnn(indata=x, out_channel=64, training=self.training, k=3, stride=2,
-            #                  scope="res_cnn_5")  # [N,24,256]
-            # x = residual_cnn(indata=x, out_channel=64, training=self.training, k=3, stride=1,
-            #                  scope="res_cnn_6")  # [N,24,256]
-
-            # x = residual_cnn(indata=x, out_channel=128, training=self.training, k=3, stride=2,
-            #                  scope="res_cnn_7")  # [N,12,512]
-            # x = residual_cnn(indata=x, out_channel=128, training=self.training, k=3, stride=1,
-            #                  scope="res_cnn_8")  # [N,12,512]
-
-            # x = residual_cnn(indata=x, out_channel=256, training=self.training, k=3, stride=2,
-            #                  scope="res_cnn_9")  # [N,6,1024]
-            # x = residual_cnn(indata=x, out_channel=256, training=self.training, k=3, stride=1,
-            #                  scope="res_cnn_10")  # [N,6,1024]
-            # x_shape = x.get_shape().as_list()
-            # signal_model_output = tf.reshape(x, [x_shape[0], x_shape[2] * x_shape[3]])  # [batch,6*256]
+            # input_signal = signals
+            # signal_model_output = self.signal_model(input_signal)
+            signal_model_output = None
 
         with tf.name_scope("Joint_model"):
-            joint_input = tf.concat(
-                [event_model_output, signal_model_output], axis=1)  # [batch,1536+256*2]
-            joint_input_shape = joint_input.get_shape().as_list()
-            fc1 = Fully_connected(
-                joint_input, out_num=joint_input_shape[1], layer_name='joint_model_fc1')
-            drop1 = tf.nn.dropout(fc1, keep_prob=self.keep_prob)
-            fc2 = Fully_connected(
-                drop1, class_num, layer_name="joint_model_fc2")
-            drop2 = tf.nn.dropout(fc2, keep_prob=self.keep_prob)
-            logits = drop2
+            logits = self.join_model(event_model_output, signal_model_output)
         with tf.name_scope("train_opts"):
             self.activation_logits = tf.nn.sigmoid(logits)
             self.cost = tf.nn.sigmoid_cross_entropy_with_logits(
@@ -285,22 +146,118 @@ class Model():
             self.train_opt = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(self.loss,
                                                                                     global_step=self.global_step)
             self.prediction = tf.argmax(self.activation_logits, axis=1)
-            # self.accuracy = tf.reduce_mean(
-            #     tf.cast(tf.equal(self.prediction, tf.argmax(one_hot_labels, axis=1)), dtype=tf.float32))
-            # TODO : precision and recall
-            # self.accuracy, self.accuracy_op = tf.metrics.accuracy(
-            #     labels=self.labels, predictions=self.prediction, name="valid_metrics")
-            # self.precision, self.precision_op = tf.metrics.precision(
-            #     labels=self.labels, predictions=self.prediction, name="valid_metrics")
-            # self.recall, self.recall_op = tf.metrics.recall(
-            #     labels=self.labels, predictions=self.prediction, name="valid_metrics")
-            # self.auc, self.auc_op = tf.metrics.auc(
-            #     labels=self.labels, predictions=self.prediction, name="valid_metrics")
-            # validation_vars = tf.get_collection(tf.GraphKeys.LOCAL_VARIABLES)
-            # self.running_validation_vars_init = tf.variables_initializer(
-            #     var_list=validation_vars)
-            # tf.summary.scalar('loss', self.loss)
-            # tf.summary.scalar('accuarcy', self.accuracy)
-            # tf.summary.scalar('recall', self.recall)
-            # tf.summary.scalar('precision', self.precision)
-            # tf.summary.scalar('auc', self.auc)
+
+
+class Event_model():
+    """
+    A multi layer bidirectional recurent neural network
+    """
+
+    def __init__(self,
+                 sequence_len,
+                 layer_num,
+                 hidden_num,
+                 keep_prob):
+        self.seq_length = sequence_len
+        self.layer_num = layer_num
+        self.hidden_num = hidden_num
+        self.keep_prob = keep_prob
+
+    def __call__(self, input):
+        rnn_out, rnn_states = rnn_layers(input,
+                                         seq_length=self.seq_length,
+                                         hidden_num=self.hidden_num,
+                                         layer_num=self.layer_num,
+                                         kprob=self.keep_prob)
+        fw_out = rnn_out[0]
+        bw_out = rnn_out[1]
+        extract_rnn_out = tf.concat(
+            [rnn_out[0][:, -1, :], rnn_out[1][:, 0, :]], axis=1)
+        return extract_rnn_out
+
+
+class incept_net():
+    def __init__(self):
+        pass
+
+    def __call__(self, signals):
+        input_signal = signals
+        with tf.variable_scope("conv_layer1"):
+            x = tf.layers.conv2d(inputs=input_signal, filters=64, kernel_size=[1, 7], strides=2, padding="SAME",
+                                 use_bias=False, name="conv")
+            x = Batch_Normalization(
+                x, is_training=self.training, scope='bn')
+            x = tf.nn.relu(x)  # [188,64]
+        with tf.variable_scope("maxpool_layer1"):
+            x = tf.layers.max_pooling2d(
+                x, [1, 3], strides=2, padding="SAME", name="maxpool")  # [94,64]
+        with tf.variable_scope("conv_layer2"):
+            x = tf.layers.conv2d(inputs=x, filters=128, kernel_size=[1, 1], strides=1, padding="SAME",
+                                 use_bias=False, name="conv")
+            x = Batch_Normalization(
+                x, is_training=self.training, scope='bn')
+            x = tf.nn.relu(x)  # [94,128]
+        with tf.variable_scope("conv_layer3"):
+            x = tf.layers.conv2d(inputs=x, filters=256, kernel_size=[1, 3], strides=1, padding="SAME",
+                                 use_bias=False, name="conv")
+            x = Batch_Normalization(
+                x, is_training=self.training, scope='bn')
+            x = tf.nn.relu(x)  # [94,256]
+        # inception layer x 11
+        with tf.variable_scope('incp_layer1'):
+            x = inception_layer(x, self.training)  # [94,192]
+        with tf.variable_scope('incp_layer2'):
+            x = inception_layer(x, self.training)
+        with tf.variable_scope('incp_layer3'):
+            x = inception_layer(x, self.training)
+        with tf.variable_scope('maxpool_layer2'):
+            x = tf.layers.max_pooling2d(
+                x, [1, 3], strides=2, padding="SAME", name="maxpool")   # [47,192]
+        with tf.variable_scope('incp_layer4'):
+            x = inception_layer(x, self.training)
+        with tf.variable_scope('incp_layer5'):
+            x = inception_layer(x, self.training)
+        with tf.variable_scope('incp_layer6'):
+            x = inception_layer(x, self.training)
+        with tf.variable_scope('incp_layer7'):
+            x = inception_layer(x, self.training)
+        with tf.variable_scope('incp_layer8'):
+            x = inception_layer(x, self.training)
+        with tf.variable_scope('maxpool_layer3'):
+            x = tf.layers.max_pooling2d(
+                x, [1, 3], strides=2, padding="SAME", name="maxpool")   # [24,192]
+        with tf.variable_scope('incp_layer9'):
+            x = inception_layer(x, self.training)
+        with tf.variable_scope('incp_layer10'):
+            x = inception_layer(x, self.training)
+        with tf.variable_scope('incp_layer11'):
+            x = inception_layer(x, self.training)
+        with tf.variable_scope('avgpool_layer1'):
+            x = tf.layers.average_pooling2d(
+                x, [1, 7], strides=1, padding="SAME", name="avgpool")   # [24,192]
+        # print('inception output shape:', x.get_shape().as_list())
+        x_shape = x.get_shape().as_list()
+        signal_model_output = tf.reshape(x, [-1, x_shape[2] * x_shape[3]])
+        return signal_model_output
+
+
+class Joint_model():
+    def __init__(self, output_hidden, keep_prob):
+        self.output_hidden = output_hidden
+        self.keep_prob = keep_prob
+
+    def __call__(self, event_model_output, signal_model_output):
+        if signal_model_output is not None:
+            joint_input = tf.concat(
+                [event_model_output, signal_model_output], axis=1)  # [batch,1536+256*2]
+        else:
+            joint_input = event_model_output
+
+        joint_input_shape = joint_input.get_shape().as_list()
+        fc1 = Fully_connected(
+            joint_input, out_num=joint_input_shape[1], layer_name='joint_model_fc1')
+        drop1 = tf.nn.dropout(fc1, keep_prob=self.keep_prob)
+        fc2 = Fully_connected(
+            drop1, self.output_hidden, layer_name="joint_model_fc2")
+        drop2 = tf.nn.dropout(fc2, keep_prob=self.keep_prob)
+        return drop2
