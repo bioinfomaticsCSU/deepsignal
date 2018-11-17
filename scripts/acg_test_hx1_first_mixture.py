83c96c3ae015ec11c6c1362a6810caf97827edbe
hn <csuhuangneng@gmail.com>
2018-11-16 20:33:18 -0500

    更改后信号的长度位360

--- scripts/acg_test_hx1_first_mixture.py
+++ scripts/acg_test_hx1_first_mixture.py
@@ -12,7 +12,7 @@ for subdir in os.listdir(test_dir):
     out_name = os.path.join(result_path, '.'.join(subdir.split('.'))+'.result')
     # print(test_path)
     # print(out_name)
-    cmd = "CUDA_VISIBLE_DEVICES=1 python ../predict.py -i {input}/ -o {model}/ -r {output} -n {modelname} -x 17 -y 375 -z 60 ".format(
+    cmd = "CUDA_VISIBLE_DEVICES=1 python ../predict.py -i {input}/ -o {model}/ -r {output} -n {modelname} -x 17 -y 360 -z 60 ".format(
         input=test_path, model=model_path, modelname=model_name, output=out_name)
     print(cmd)
     os.system(cmd)
