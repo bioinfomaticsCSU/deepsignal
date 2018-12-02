ee919ef37983f0851495303cc52e76dbc370959c
hn <huangneng@csu.edu.cn>
2018-12-02 15:02:01 -0500

    add ecoli 2 hx1 first 10x

---
+++ scripts/acg_test_hx2_first_mixture.py
@@ -0,0 +1,18 @@
+import os
+
+model_path = "/home/huangneng/master_deepsignal/deepsignal/last_v_models/ecoli2human_tem_bn17_sn360_acg"
+result_path = "/home/huangneng/master_deepsignal/deepsignal/last_v_results/acg_ecoli_mixture_1"
+test_dir = "/home/huangneng/data/human_hx1.norm.mad/test.bn17.sn360.5xs/BJXWZ.1.C789101112D1E13"
+model_name = '6'
+
+for subdir in os.listdir(test_dir):
+    if not subdir.endswith(".mcgs"):
+        continue
+    test_path = os.path.join(test_dir, subdir)
+    out_name = os.path.join(result_path, '.'.join(subdir.split('.'))+'.result')
+    # print(test_path)
+    # print(out_name)
+    cmd = "CUDA_VISIBLE_DEVICES=3 python ../predict.py -i {input}/ -o {model}/ -r {output} -n {modelname} -x 17 -y 360 -z 60 ".format(
+        input=test_path, model=model_path, modelname=model_name, output=out_name)
+    print(cmd)
+    os.system(cmd)
